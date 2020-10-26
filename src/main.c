fa7a39a897e028eabc7546b8caa9fcfe5d2f46b1
NProof <34260623+NProof@users.noreply.github.com>
2020-10-26 13:33:59 +0800

    Test mapping function

--- src/main.c
+++ src/main.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
 #include "../header/Pyramid.h"
+#include "../header/Maze.h"
 
 int main(int argc, char ** argv) {
     FILE * fp;
@@ -19,6 +22,32 @@ int main(int argc, char ** argv) {
         readData(&pyramid, fp);
         fclose(fp);
 
-        printMap(&pyramid);
+//        printMap(&pyramid);
+
+        int j, k, l;
+        for(int i = 0; i < pyramid.nodes; ++i) {
+            linearToPmd(i, pyramid.n, &j, &k, &l);
+//            printf(" %2d -> ( %2d, %2d, %2d )", i, l, k ,j);
+            int tmp = -1;
+            pmdToLinear(&tmp, pyramid.n, j, k, l);
+//            printf(" -> %2d\n", tmp);
+            if(i != tmp)
+                printf("FAILED in %d\n", i);
+        }
+
+        int pass = 0;
+        for(j = 0; j < pyramid.n; ++j) {
+            for(k = 0; k < pyramid.n; ++k) {
+                for(l = 0; l < pyramid.n; ++l) {
+                    int tmp = -1;
+                    if(pmdToLinear(&tmp, pyramid.n, j, k, l)==true) {
+                        ++pass;
+//                        printf("( %2d, %2d, %2d ) -> %2d\n", l, k, j, tmp);
+                    }
+                }
+            }
+        }
+        if (pass!=pyramid.nodes)
+            printf("FAILED");
     }
 }
